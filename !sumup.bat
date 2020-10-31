b03f77d4c6167248943eb2ef9beecec238ccf36d
860230 <mpaushev2@mail.ru>
2020-11-01 01:30:25 +0300

    Update !sumup.bat

--- !sumup.bat
+++ !sumup.bat
@@ -1,5 +1,5 @@
 @echo off
-del !oldsum.txt
-copy !sum.txt !oldsum.txt
-del !sum.txt
-copy src\*.* + include\*.* !sum.txt
+del oldsum.txt
+copy sum.txt oldsum.txt
+del sum.txt
+copy src\*.* + include\*.* sum.txt
